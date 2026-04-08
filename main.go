d832dfbb29db50089c32202f69a2d6dcb2adb36c
egevtech <egevtech@gmail.com>
2026-04-09 00:02:47 +0300

    Full refactor. Parsing arguments with go-arg, new response system and more

--- main.go
+++ main.go
@@ -1,123 +1,37 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"io"
+	"log"
 	"net/http"
-	"os"
-	"strings"
+
+	"github.com/alexflint/go-arg"
 )
 
 type ResponseType string
 
-const (
-	HTTPS ResponseType = "https"
-	HTTP  ResponseType = "http"
-)
+var args struct {
+	Addr string `arg:"positional,required" help:"Address to ping"`
+}
 
 func main() {
+	arg.MustParse(&args)
 
-	var addr string
-	var RType ResponseType
-	var commands []string
+	resp, err := http.Get(args.Addr)
 
-	addr = "localhost:8080"
-	RType = HTTP
-
-	help := "|rtype <type>| where type=={https, http} - select response type\n" +
-		"|connect <addr>| - set address, (for ex. 'connect localhost:8080')\n" +
-		"|fget <addr>| - send GET request to <addr>\n" +
-		"|get| - send GET request\n" +
-		"|exit| - exit program\n"
-
-	fmt.Print(help)
-
-	for {
-		fmt.Printf("<%s://%s/> ?", RType, addr)
-		command, _ := bufio.NewReader(os.Stdin).ReadString('\n')
-		command = command[:len(command)-1]
-
-		commands = strings.Split(command, " ")
-
-		switch commands[0] {
-		case "exit":
-			return
-
-		case "connect":
-			if len(commands) < 2 {
-				fmt.Printf("[WARNING] Not enough arguments: given %d, needed %d\n", len(commands), 2)
-				break
-			}
-
-			addr = commands[1]
-			break
-
-		case "get":
-			resp, err := http.Get(fmt.Sprintf("%s://%s/", RType, addr))
-			if err != nil {
-				fmt.Printf("[NF] Error occured: %s\n", err)
-				break
-			}
-			defer resp.Body.Close()
-
-			body, err := io.ReadAll(resp.Body)
-			if err != nil {
-				fmt.Printf("[NF] Error occured while reading answer: %s\n", err)
-				break
-			}
-			fmt.Println(string(body))
-			break
-
-		case "fget":
-			if len(commands) < 2 {
-				fmt.Printf("[WARNING] Not enough arguments: given %d, needed %d\n", len(commands), 3)
-				break
-			}
-
-			t_addr := commands[1]
-
-			fmt.Printf("Sending GET request to %s\n", t_addr)
-
-			resp, err := http.Get(fmt.Sprintf("%s://%s/", RType, t_addr))
-			if err != nil {
-				fmt.Printf("[NF] Error occured: %s\n", err)
-				break
-			}
-
-			defer resp.Body.Close()
-
-			body, err := io.ReadAll(resp.Body)
-			if err != nil {
-				fmt.Printf("[NF] Error occured while reading answer: %s\n", err)
-				break
-			}
-
-			fmt.Println(string(body))
-			break
-
-		case "rtype":
-			if len(commands) < 2 {
-				fmt.Printf("[WARNING] Not enough arguments: given %d, needed %d\n", len(commands), 2)
-				break
-			}
-
-			switch commands[1] {
-			case "https":
-				RType = HTTPS
-				break
-			case "http":
-				RType = HTTP
-				break
-			}
-
-		case "help":
-			fmt.Print(help)
-			break
-
-		default:
-			break
-		}
+	if err != nil {
+		log.Fatalf("Error to connect: %s", err)
+		return
 	}
 
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalf("Failed to read answer body: %s", err)
+		return
+	}
+
+	fmt.Println(string(body))
 }
